fe7c322cd368048656982ac6c0a8a740204745db
Richard Holland <richard.holland@starstone.co.nz>
2021-07-23 23:27:27 +0200

    proof and hash routines, still incomplete

--- verify.py
+++ verify.py
@@ -1,10 +1,82 @@
 import sys
 import json
 import xrpl
+import hashlib
+from binascii  import hexlify, unhexlify
+import math
+
+#const prefix_LWR = '4C575200'
+#const prefix_SND = '534E4400'
+#const prefix_MIN = '4D494E00'
+#const prefix_TXN = '54584E00'
 
 def err(e):
     sys.stderr.write("Error: " + e)
     return False
+
+def make_vl_bytes(l):
+    if type(l) == float:
+        l = ceil(l)
+    if type(l) != int:
+        return False
+    if l <= 192:
+        return bytes([l])
+    elif l <= 12480:
+        b1 = floor((l - 193) / 256 + 193)
+        return bytes([b1, l - 193 - 256 * (b1 - 193)])
+    elif l <= 918744:
+        b1 = floor((l - 12481) / 65536 + 241)
+        b2 = floor((l - 12481 - 65536 * (b1 - 241)) / 256)
+        return bytes([b1, b2, l - 12481 - 65536 * (b1 - 241) - 256 * b2])
+    else:
+        return err("Cannot generate vl for length = " + str(l) + ", too large")
+
+def sha512h(x):
+    m = hashlib.sha512()
+    m.update(x)
+    return m.digest()[:32]
+
+def hash_txn(txn):
+    if type(txn) == str:
+        txn = unhexlify(txn)
+    return sha512h(b'TXN\x00' + txn)
+
+def hash_txn_and_meta(txn, meta):
+    if type(txn) == str:
+        txn = unhexlify(txn)
+
+    if type(meta) == str:
+        meta = unhexlify(meta)
+
+    vl1 = make_vl_bytes(len(txn))
+    vl2 = make_vl_bytes(len(meta))
+    
+    if vl1 == False or vl2 == False:
+        return False
+
+    return sha512h(b'SND\x00' + vl1 + txn + vl2 + meta + hash_txn(txn))
+
+
+def hash_proof(proof):
+    if type(proof) != list:
+        return err('Proof must be a list')
+    
+    if len(proof) < 16:
+        return False
+
+    hasher = hashlib.sha512()
+    hasher.update(b'MIN\x00')
+
+    for i in range(16):
+        if type(proof[i]) == str:
+            hasher.update(unhexlify(proof[i]))
+        elif type(proof[i]) == list:
+            hasher.update(hash_proof(proof[i]))
+        else:
+            return err("Unknown object in proof list")
+
+    return hasher.digest()[:32]
+    
 
 def verify(xpop):
     if type(xpop) == str:
@@ -29,6 +101,12 @@ def verify(xpop):
     validation = xpop["validation"]
     transaction = xpop["transaction"]
 
+    print("ledger")
+    print(ledger)
+
+    print("proof")
+    print(hexlify(hash_proof(transaction["proof"])))
+
     # RH: TODO actual validation here
 
     try:
@@ -37,11 +115,14 @@ def verify(xpop):
     except:
         return err("Error decoding txblob and meta")
 
-    return (tx, meta)
+    return hash_txn(transaction["blob"])
+    #return (tx, meta)
+
+#print(sha512h(b''))
 
 xpop = ''
 for line in sys.stdin:
     xpop += line.rstrip()
 
-
-print(verify(xpop))
+#print(
+verify(xpop)
